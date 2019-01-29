428b5b6b46ad3415efb30871acd0172c34977144
dvte001 <dvte001@PLO-WS060>
2019-01-29 10:34:39 +0200

    Add project files.

---
+++ Problem 1.Basic Stack Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_1.Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
